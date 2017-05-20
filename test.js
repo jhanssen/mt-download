15ed0c0a4e5b349fbab46309de5035fe8ecff3b3
Jan Erik Hanssen <jhanssen@gmail.com>
2017-05-20 15:34:46 -0700

    stuffs

--- test.js
+++ test.js
@@ -1,17 +1,33 @@
-/*global require,process*/
+/*global require,process,setTimeout*/
 
 const mtdl = require("./index");
-let url = "http://releases.ubuntu.com/16.04/SHA256SUMS.gpg";
-//let url = "http://releases.ubuntu.com/16.04/ubuntu-16.04.2-server-i386.template";
+//let url = "http://releases.ubuntu.com/16.04/SHA256SUMS.gpg";
+let url = "http://releases.ubuntu.com/16.04/ubuntu-16.04.2-server-i386.template";
+let ctrl;
 mtdl({ url: url, path: "." })
     .then((obj) => {
-        obj.on("finished", () => {
+        ctrl = obj;
+        ctrl.on("finished", () => {
             console.log("finito");
-            process.exit();
+            //process.exit();
         });
-        obj.start();
-        console.log(obj);
+        ctrl.start();
+        console.log(ctrl);
         // process.exit();
-    }).catch(() => {
-        process.exit();
+    }).catch((err) => {
+        console.log(err);
+        //process.exit();
     });
+
+// (function wait () {
+//     console.log(ctrl);
+//     setTimeout(wait, 1000);
+// })();
+
+process.on('uncaughtException', function (err) {
+    console.log(err, ctrl);
+});
+
+process.on("exit", function(code) {
+    console.log("about to exit", code, ctrl);
+});
